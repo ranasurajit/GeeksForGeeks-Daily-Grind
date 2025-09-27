8f09ac584c310baae4dde3e861428cd9b7583b3f
Surajit Rana <surajit1602@gmail.com>
2025-09-27 23:38:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
@@ -1,26 +1,30 @@
 class Solution {
+    /**
+     * Approach I : Using Extra Space Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     */
     public int kBitFlips(int[] arr, int k) {
         int n = arr.length;
-
         int flips = 0;
-        boolean[] isFlipped = new boolean[n];
-        int flipCountFromPastForCurri = 0;
-
-        for (int i = 0; i < n; i++) {
+        boolean[] isFlipped = new boolean[n]; // SC: O(N)
+        int flipCountFromPast = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
             if (i >= k && isFlipped[i - k]) {
-                flipCountFromPastForCurri--;
+                // decrement extra count
+                flipCountFromPast--;
             }
-
-            if (flipCountFromPastForCurri % 2 == arr[i]) {
+            if (flipCountFromPast % 2 == arr[i]) {
                 if (i + k > n) {
+                    // not possible to make array 'arr' elements 1
                     return -1;
                 }
-                flipCountFromPastForCurri++;
-                isFlipped[i] = true;
+                flipCountFromPast++;
                 flips++;
+                isFlipped[i] = true;
             }
         }
-
         return flips;
     }
 }
