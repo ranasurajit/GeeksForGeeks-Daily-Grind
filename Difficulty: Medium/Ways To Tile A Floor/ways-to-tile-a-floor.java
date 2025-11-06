df414e340a11577a0813a459681787f1c17f6aa7
Surajit Rana <surajit1602@gmail.com>
2025-11-06 14:32:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways To Tile A Floor/ways-to-tile-a-floor.java
+++ Difficulty: Medium/Ways To Tile A Floor/ways-to-tile-a-floor.java
@@ -1,5 +1,55 @@
 class Solution {
     /**
+     * Approach IV : Using Space Optimization (Optimized DP) Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     * 
+     * Accepted (45 / 45 Testcases Passed)
+     */
+    public int numberOfWays(int n) {
+        // n == 1, number of ways = 1, n = 2, number of ways = 2
+        // f(n) = f(n - 1) + f(n - 2)
+        if (n <= 2) {
+            return n;
+        }
+        int prev2 = 1;
+        int prev = 2;
+        for (int i = 3; i <= n; i++) { // TC: O(N)
+            int current = prev + prev2;
+            prev2 = prev;
+            prev = current;
+        }
+        return prev;
+    }
+
+    /**
+     * Approach III : Using Tabulation Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * - O(N) - dp memory
+     * 
+     * Accepted (45 / 45 Testcases Passed)
+     */
+    public int numberOfWaysTabulation(int n) {
+        // n == 1, number of ways = 1, n = 2, number of ways = 2
+        // f(n) = f(n - 1) + f(n - 2)
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1]; // SC: O(N)
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) { // TC: O(N)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    /**
      * Approach II : Using Memoization Approach
      * 
      * TC: O(N)
@@ -7,9 +57,9 @@ class Solution {
      * - O(N) - recursion stack space
      * - O(N) - memoization memory
      * 
-     * Time Limit Exceeded
+     * Accepted (45 / 45 Testcases Passed)
      */
-    public int numberOfWays(int n) {
+    public int numberOfWaysMemoization(int n) {
         // n == 1, number of ways = 1, n = 2, number of ways = 2
         // f(n) = f(n - 1) + f(n - 2)
         int[] memo = new int[n + 1]; // SC: O(N)
@@ -43,7 +93,7 @@ class Solution {
      * SC: O(N)
      * - O(N) - recursion stack space
      * 
-     * Time Limit Exceeded
+     * Time Limit Exceeded (0 / 45 Testcases Passed)
      */
     public int numberOfWaysRecursion(int n) {
         // n == 1, number of ways = 1, n = 2, number of ways = 2
