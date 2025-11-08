f84befc3ae908a923114893e89d68e46b09fb494
Surajit Rana <surajit1602@gmail.com>
2025-11-08 15:45:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.java
+++ Difficulty: Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.java
@@ -3,6 +3,74 @@ class Solution {
     private int m;
     
     /**
+     * Approach IV : Using Space Optimization Approach
+     * 
+     * TC: O(N x M x K)
+     * SC: O(M x K) + O(M x K) ~ O(M x K)
+     * 
+     * - O(M x K) - prev and current memory
+     * 
+     * Accepted (1121 /1121 testcases passed)
+     */
+    public int numberOfPath(int[][] mat, int k) {
+        this.n = mat.length;
+        this.m = mat[0].length;
+        // Initialization
+        int[][] prev = new int[m][k + 1]; // SC: O(M x K)
+        // Iterative Calls
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int[][] current = new int[m][k + 1]; // SC: O(M x K)
+            for (int j = 0; j < m; j++) { // TC: O(M)
+                for (int p = mat[i][j]; p <= k; p++) { // TC: O(K)
+                    int rightMove = (j > 0 && p >= mat[i][j]) ? current[j - 1][p - mat[i][j]] : 0;
+                    int downMove = (i > 0 && p >= mat[i][j]) ? prev[j][p - mat[i][j]] : 0;
+                    if (i == 0 && j == 0 && mat[0][0] == p) {
+                        current[j][p] = 1;
+                    } else {
+                        current[j][p] = rightMove + downMove;
+                    }
+                }
+            }
+            prev = current;
+        }
+        return prev[m - 1][k];
+    }
+
+    /**
+     * Approach III : Using Tabulation Approach
+     * 
+     * TC: O(N x M x K)
+     * SC: O(N x M x K)
+     * 
+     * - O(N x M x K) - dp memory
+     * 
+     * Accepted (1121 /1121 testcases passed)
+     */
+    public int numberOfPathTabulation(int[][] mat, int k) {
+        this.n = mat.length;
+        this.m = mat[0].length;
+        // Initialization
+        int[][][] dp = new int[n][m][k + 1]; // SC: O(N x M x K)
+        if (mat[0][0] <= k) {
+            dp[0][0][mat[0][0]] = 1;
+        }
+        // Iterative Calls
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            for (int j = 0; j < m; j++) { // TC: O(M)
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                for (int p = mat[i][j]; p <= k; p++) { // TC: O(K)
+                    int rightMove = (j > 0 && p >= mat[i][j]) ? dp[i][j - 1][p - mat[i][j]] : 0;
+                    int downMove = (i > 0 && p >= mat[i][j]) ? dp[i - 1][j][p - mat[i][j]] : 0;
+                    dp[i][j][p] = rightMove + downMove;
+                }
+            }
+        }
+        return dp[n - 1][m - 1][k];
+    }
+
+    /**
      * Approach II : Using Memoization Approach
      * 
      * TC: O(N x M x K)
@@ -13,7 +81,7 @@ class Solution {
      * 
      * Accepted (1121 /1121 testcases passed)
      */
-    public int numberOfPath(int[][] mat, int k) {
+    public int numberOfPathMemoization(int[][] mat, int k) {
         this.n = mat.length;
         this.m = mat[0].length;
         int[][][] memo = new int[n][m][k + 1]; // SC: O(N x M x K)
