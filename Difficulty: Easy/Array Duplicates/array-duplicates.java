00d11fab2b21834873587845deb7ba5dfeebfcd6
Surajit Rana <surajit1602@gmail.com>
2025-12-09 20:16:54 +0530

    Update array-duplicates.java

--- Difficulty: Easy/Array Duplicates/array-duplicates.java
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -1,11 +1,32 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Optimal (Without Extra Space) Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int x = Math.abs(arr[i]);
+            int index = x - 1;
+            if (arr[index] < 0) {
+                result.add(x);
+            } else {
+                arr[index] = -1 * arr[index];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Hashing (With Extra Space) Approach
      * 
      * TC: O(N)
      * SC: O(N)
      */
-    public ArrayList<Integer> findDuplicates(int[] arr) {
+    public ArrayList<Integer> findDuplicatesBruteForce(int[] arr) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         Set<Integer> set = new HashSet<Integer>(); // SC: O(N)
         for (int num : arr) { // TC: O(N)
