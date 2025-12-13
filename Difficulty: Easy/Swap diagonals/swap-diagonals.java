075e5751afa4e5e46a21866bb1c49b79bc7fe5ab
Surajit Rana <surajit1602@gmail.com>
2025-12-13 12:25:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap diagonals/swap-diagonals.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * Approach : Using Array Simulation Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public void swapDiagonal(int[][] mat) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            // swapping diagonal cell values
+            int temp = mat[i][n - 1 - i];
+            mat[i][n - 1 - i] = mat[i][i];
+            mat[i][i] = temp;
+        }
+    }
+}
