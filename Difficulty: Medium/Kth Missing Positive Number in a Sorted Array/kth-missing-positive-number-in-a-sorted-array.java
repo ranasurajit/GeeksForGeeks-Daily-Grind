a855c9a3644f8472bdd9aa98e77d581674ae09fe
Surajit Rana <surajit1602@gmail.com>
2025-12-26 15:57:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,11 +1,44 @@
 class Solution {
     /**
-     * Approach : Using Binary Search on Answers Approach
+     * Approach II : Using Binary Search on Answers Approach
      * 
-     * TC: O(log(Max(arr)) x log(N)) ~ O(log(Max(arr)) + log(N)) ~ O(log(N))
+     * TC: O(log(N))
      * SC: O(1)
      */
     public int kthMissing(int[] arr, int k) {
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int index = -1;
+        /**
+         * Number missing till index i = arr[i] - (i + 1)
+         */
+        while (low <= high) { // TC: O(log(N))
+            int mid = low + (high - low) / 2;
+            int missingNumbersTillMid = arr[mid] - (mid + 1);
+            if (missingNumbersTillMid >= k) {
+                high = mid - 1;
+            } else {
+                index = mid;
+                low = mid + 1;
+            }
+        }
+        if (index == -1) {
+            // index -1 means kth missing is before arr[0]
+            return k;
+        }
+        // missing elements till index
+        int missingElements = arr[index] - (index + 1);
+        return arr[index] + k - missingElements;
+    }
+
+    /**
+     * Approach I : Using Binary Search on Answers Approach
+     * 
+     * TC: O(log(Max(arr)) x log(N))
+     * SC: O(1)
+     */
+    public int kthMissingApproachI(int[] arr, int k) {
         int n = arr.length;
         int low = 1; // minimum positive number
         int high = arr[n - 1] + k; // maximum positive number
