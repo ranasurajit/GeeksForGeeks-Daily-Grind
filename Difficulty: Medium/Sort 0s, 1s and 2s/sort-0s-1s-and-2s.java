ddf2a64e6568cc0cb466803485bd9aa5106ba201
Surajit Rana <surajit1602@gmail.com>
2026-01-02 00:13:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * Approach I : Using Array Sorting Approach
+     * 
+     * TC: O(N x log(N))
+     * SC: O(1)
+     */
+    public void sort012(int[] arr) {
+        Arrays.sort(arr); // TC: O(N x log(N))
+    }
+}
