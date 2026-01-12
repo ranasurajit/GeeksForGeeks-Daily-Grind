5bf43e0b50786c76d1aea5e08716413342639b78
Surajit Rana <surajit1602@gmail.com>
2026-01-12 10:52:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -7,22 +7,32 @@ class Solution {
      */
     public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
         int n = arr.length;
-        int i = 0; // pointer at the start of sliding window
-        int j = 0; // pointer at the end of sliding window
-        // we will try to keep maximum always at the front of Deque
-        Deque<Integer> deque = new ArrayDeque<Integer>(); // SC: O(K)
+        int i = 0; // start pointer of sliding window 
+        int j = 0; // end pointer of sliding window
         ArrayList<Integer> result = new ArrayList<Integer>();
+        /**
+         * the intuition is to always keep the maximum within 
+         * the valid sliding window at the beginning / front
+         * of the data-strcuture so that we can get it in O(1)
+         * time complexity and Deque is more suitable here and
+         * to make the ease in sliding to next window and clean
+         * up invalid indices, we can push indices in the Deque
+         */
+        Deque<Integer> deque = new ArrayDeque<Integer>(); // SC: O(K)
         while (j < n) { // TC: O(N)
             while (!deque.isEmpty() && arr[j] >= arr[deque.peekLast()]) {
                 deque.pollLast();
             }
             deque.offerLast(j);
             if (j - i + 1 == k) {
+                // sliding window size is met, so peek the maximum
                 result.add(arr[deque.peekFirst()]);
                 // remove computation from index 'i'
                 if (i == deque.peekFirst()) {
+                    // since ith index has the maximum in deque so remove it
                     deque.pollFirst();
                 }
+                // slide to next window
                 i++;
             }
             j++;
