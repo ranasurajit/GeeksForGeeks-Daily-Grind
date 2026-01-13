5819df4f24fcd36b29a22d76f1959babbb3c6e3a
Surajit Rana <surajit1602@gmail.com>
2026-01-13 11:42:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bus Ticket Change/bus-ticket-change.java
+++ Difficulty: Easy/Bus Ticket Change/bus-ticket-change.java
@@ -3,16 +3,12 @@ class Solution {
      * Approach : Using Hashing + Array Simulation Approach
      * 
      * TC: O(N)
-     * SC: O(3) ~ O(1)
+     * SC: O(2) ~ O(1)
      */
     public boolean canServe(int[] arr) {
         int n = arr.length;
-        if (arr[0] > 5) {
-            // assuming I don't have any denominations initially
-            return false;
-        }
-        // In the below array we can store denominations of 5, 10 and 20 respectively
-        int[] denominations = { 0, 0, 0 }; // SC: O(3)
+        // In the below array we can store denominations of 5 and 10 respectively
+        int[] denominations = { 0, 0 }; // SC: O(2)
         int i = 0;
         for (i = 0; i < n; i++) { // TC: O(N)
             if (arr[i] == 5) {
@@ -28,12 +24,10 @@ class Solution {
             } else {
                 if (denominations[1] > 0 && denominations[0] > 0) {
                     // returning 1 denomination of 5 and 1 denomination of 10
-                    denominations[2]++;
                     denominations[0]--;
                     denominations[1]--;
                 } else if (denominations[0] > 2) {
                     // returning 3 denominations of 5
-                    denominations[2]++;
                     denominations[0] -= 3;
                 } else {
                     break;
