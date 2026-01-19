eb3ee71b050771b524c4c9f6259bfa83dcb60b58
Surajit Rana <surajit1602@gmail.com>
2026-01-19 14:48:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove K Digits/remove-k-digits.java
+++ Difficulty: Medium/Remove K Digits/remove-k-digits.java
@@ -1,11 +1,50 @@
 class Solution {
     /**
-     * Approach I : Using Monotonic Stack Approach
+     * Approach II : Using Monotonic Deque Approach
      * 
      * TC: O(N) + O(N - K) ~ O(N)
      * SC: O(N) + O(N - K) ~ O(N)
      */
     public String removeKdig(String s, int k) {
+        int n = s.length();
+        if (k == n) {
+            return "0";
+        }
+        // we will store the characters in the Deque
+        Deque<Character> deque = new ArrayDeque<Character>(); // SC: O(N)
+        int i = 0;
+        while (i < n) { // TC: O(N)
+            char ch = s.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peek() > ch) {
+                deque.pop();
+                k--;
+            }
+            deque.push(ch);
+            i++;
+        }
+        while (!deque.isEmpty() && k > 0) {
+            deque.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder(); // SC: O(N - K)
+        while (!deque.isEmpty()) { // TC: O(N - K)
+            sb.append(deque.pop());
+        }
+        sb = sb.reverse();
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            // only remove leading zeros if length > 1
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Approach I : Using Monotonic Stack Approach
+     * 
+     * TC: O(N) + O(N - K) ~ O(N)
+     * SC: O(N) + O(N - K) ~ O(N)
+     */
+    public String removeKdigUsingStack(String s, int k) {
         int n = s.length();
         if (k == n) {
             return "0";
