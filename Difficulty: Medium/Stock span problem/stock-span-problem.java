565ba7781db48e919a5b766bdf250c72b84467d6
Surajit Rana <surajit1602@gmail.com>
2026-01-21 09:45:18 +0530

    Update stock-span-problem.java

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -7,30 +7,25 @@ class Solution {
      */
     public ArrayList<Integer> calculateSpan(int[] arr) {
         int n = arr.length;
-        /**
-         * Intuition: Here we need to pre-compute the 
-         * previous greater element which will basically
-         * give the span in the left till which an element
-         * is greater than it
-         * 
-         * so, for arr[j], if i is the index of previous 
-         * greater element, so span = j - (i + 1) + 1 
-         */
+        // we need to find the previous greater element
         Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
-        int[] pge = new int[n];                   // SC: O(N)
-        for (int i = 0; i < n; i++) {             // TC: O(N)
+        int[] result = new int[n];    // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
             while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
                 st.pop();
             }
-            pge[i] = st.isEmpty() ? -1 : st.peek();
+            result[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
-        ArrayList<Integer> spanList = new ArrayList<Integer>();
-        for (int right = 0; right < n; right++) { // TC: O(N)
-            int left = pge[right] + 1;
-            // span = right - left + 1
-            spanList.add(right - left + 1);
+        ArrayList<Integer> span = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            /**
+             * result[i] + 1 is the index next to previous greater element so number 
+             * of elements in span is current index (i) - (result[i] + 1) + 1
+             * which is the window of the span
+             */
+            span.add(i - (result[i] + 1) + 1);
         }
-        return spanList;
+        return span;
     }
 }
