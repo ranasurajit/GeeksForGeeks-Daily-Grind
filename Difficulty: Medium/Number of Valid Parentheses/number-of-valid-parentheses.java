5d1d9a4cfd76a73b5992024adc1c869e0552f804
Surajit Rana <surajit1602@gmail.com>
2026-01-25 02:08:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Valid Parentheses/number-of-valid-parentheses.java
+++ Difficulty: Medium/Number of Valid Parentheses/number-of-valid-parentheses.java
@@ -1,11 +1,50 @@
 class Solution {
     /**
-     * Approach : Using Recursion Approach
+     * Approach II : Using Backtracking (To Generate Catalan Subsequences) Approach
      * 
-     * TC: O(2 ^ N)
+     * TC: O(Catalan(N / 2))
      * SC: O(N)
      */
     int findWays(int n) {
+        if ((n & 1) != 0) {
+            // n must be even to form balanced valid parentheses
+            return 0;
+        }
+        int totalPairs = n / 2;
+        int[] ways = { 0 };
+        solveBetter(0, 0, totalPairs, ways);
+        return ways[0];
+    }
+    
+    /**
+     * Using Recursion Approach
+     * 
+     * TC: O(Catalan(N / 2))
+     * SC: O(N)
+     */
+    private void solveBetter(int openUsed, int closeUsed, int total, int[] ways) {
+        // Base Case
+        if (openUsed == total && closeUsed == total) {
+            ways[0]++;
+            return;
+        }
+        // Recursion Calls
+        if (openUsed < total) {
+            solveBetter(openUsed + 1, closeUsed, total, ways);
+        }
+        if (openUsed > closeUsed) {
+            // only after open bracket we can place a closed bracket
+            solveBetter(openUsed, closeUsed + 1, total, ways);
+        }
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     * 
+     * TC: O(Catalan(N / 2))
+     * SC: O(N)
+     */
+    int findWaysUsingBasicRecursion(int n) {
         if ((n & 1) != 0) {
             // n must be even to form balanced valid parentheses
             return 0;
@@ -20,7 +59,7 @@ class Solution {
     /**
      * Using Recursion Approach
      * 
-     * TC: O(2 ^ N)
+     * TC: O(Catalan(N / 2))
      * SC: O(N)
      */
     private void solve(String current, int open, int closed, int n, int[] ways) {
