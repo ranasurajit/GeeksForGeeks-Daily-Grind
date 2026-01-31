3d503142453dcdc4889a816283e1a225782307a8
Surajit Rana <surajit1602@gmail.com>
2026-02-01 03:07:59 +0530

    Update k-sized-subarray-maximum.java

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -1,35 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Sliding Window (Fixed Size) + Monotonic Deque Approach
+     * Approach : Using Sliding Window (Fixed Size) + Deque Approach
      * 
      * TC: O(N)
      * SC: O(K)
      */
     public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
         int n = arr.length;
-        int i = 0; // start pointer of sliding window 
-        int j = 0; // end pointer of sliding window
-        ArrayList<Integer> result = new ArrayList<Integer>();
         /**
-         * the intuition is to always keep the maximum within 
-         * the valid sliding window at the beginning / front
-         * of the data-strcuture so that we can get it in O(1)
-         * time complexity and Deque is more suitable here and
-         * to make the ease in sliding to next window and clean
-         * up invalid indices, we can push indices in the Deque
+         * we need a data-structure to push and pop from both ends in O(1)
+         * time complexity. so we would try to always keep the maximum
+         * value in the front of the sliding window
+         * 
+         * to easily remove any item from invalid window, we will push
+         * index of element in Deque instead of pushing the element
          */
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
         Deque<Integer> deque = new ArrayDeque<Integer>(); // SC: O(K)
+        ArrayList<Integer> result = new ArrayList<Integer>();
         while (j < n) { // TC: O(N)
-            while (!deque.isEmpty() && arr[j] >= arr[deque.peekLast()]) {
+            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[j]) {
                 deque.pollLast();
             }
             deque.offerLast(j);
             if (j - i + 1 == k) {
-                // sliding window size is met, so peek the maximum
                 result.add(arr[deque.peekFirst()]);
-                // remove computation from index 'i'
-                if (i == deque.peekFirst()) {
-                    // since ith index has the maximum in deque so remove it
+                // remove computation from index i
+                if (deque.peekFirst() == i) {
                     deque.pollFirst();
                 }
                 // slide to next window
