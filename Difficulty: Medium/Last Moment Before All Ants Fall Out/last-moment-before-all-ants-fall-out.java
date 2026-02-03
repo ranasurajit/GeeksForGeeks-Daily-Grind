2baae3790f8fbb434695fc035daa16498370ec0c
Surajit Rana <surajit1602@gmail.com>
2026-02-04 00:44:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.java
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.java
@@ -12,6 +12,14 @@ class Solution {
          * 
          * the last ant will fell down at time which is max time 
          * any ant will take to fall at any end
+         * 
+         * 
+         * Collision has no effect on timings so it is as good
+         * as ants continuing the journey to reach either side
+         * of the wooden plank, so we need to find the maximum
+         * units travelled by any ant (at speed of 1 unit / sec)
+         * so, the maximum unit = time of the last ant to fall 
+         * out the plank
          */
         int maxLeft = 0;
         for (int x : left) { // TC: O(M)
