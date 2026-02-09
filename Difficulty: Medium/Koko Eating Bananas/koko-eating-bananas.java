0af63e31d938b9c9ccce807d9e9ba44fc6afa746
Surajit Rana <surajit1602@gmail.com>
2026-02-10 01:50:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.java
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * Approach : Using Binary Search on Answers Approach
+     * 
+     * TC: O(N x log(R)) + O(N) ~ O(N x log(R))
+     * SC: O(1)
+     * 
+     * where R = Sum(arr)
+     */
+    public int kokoEat(int[] arr, int k) {
+        int n = arr.length;
+        long low = 1L;
+        long high = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            high = Math.max(high, arr[i]);
+        }
+        while (low <= high) { // TC: O(log(R))
+            long mid = low + (high - low) / 2;
+            if (timeTakenToFinishAllPiles(arr, n, mid) > k) { // TC: O(N)
+                // Koko needs to increase the eating speed (bananas / hour)
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int) low;
+    }
+
+    /**
+     * Using Array Simulation Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private long timeTakenToFinishAllPiles(int[] arr, int n, long speed) {
+        long time = 0;
+        int i = 0;
+        while (i < n) { // TC: O(N)
+            time += (arr[i] + speed - 1) / speed;
+            i++;
+        }
+        return time;
+    }
+}
