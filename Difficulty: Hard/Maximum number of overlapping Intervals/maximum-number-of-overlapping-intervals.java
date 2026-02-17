04d54f3735896deebfb39994248073544b5b1933
Surajit Rana <surajit1602@gmail.com>
2026-02-17 11:31:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Maximum number of overlapping Intervals/maximum-number-of-overlapping-intervals.java
+++ Difficulty: Hard/Maximum number of overlapping Intervals/maximum-number-of-overlapping-intervals.java
@@ -3,7 +3,7 @@ class Solution {
     /**
      * Approach : Using Hashing + Sweep Line Algorithm Approach
      * 
-     * TC: O(N) + O(N) ~ O(N)
+     * TC: O(N x log(N)) + O(N) ~ O(N x log(N))
      * SC: O(N)
      */
     public static int overlapInt(int[][] arr) {
@@ -11,13 +11,13 @@ class Solution {
         for (int[] interval : arr) { // TC: O(N)
             int start = interval[0];
             int end = interval[1] + 1;
-            sweepMap.put(start, sweepMap.getOrDefault(start, 0) + 1);
-            sweepMap.put(end, sweepMap.getOrDefault(end, 0) - 1);
+            sweepMap.put(start, sweepMap.getOrDefault(start, 0) + 1); // TC: O(log(N))
+            sweepMap.put(end, sweepMap.getOrDefault(end, 0) - 1);     // TC: O(log(N))
         }
         int maxOverlap = 0;
         int cumulativeSum = 0;
-        for (Integer key : sweepMap.keySet()) { // TC: O(N)
-            cumulativeSum += sweepMap.get(key);
+        for (Integer values : sweepMap.values()) { // TC: O(N)
+            cumulativeSum += values;
             maxOverlap = Math.max(maxOverlap, cumulativeSum);
         }
         return maxOverlap;
