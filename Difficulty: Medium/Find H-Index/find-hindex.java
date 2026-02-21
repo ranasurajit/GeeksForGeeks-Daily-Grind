b030ed80c72461c705a8a92c7b348653e4b939ba
Surajit Rana <surajit1602@gmail.com>
2026-02-21 12:22:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,12 +1,38 @@
 class Solution {
     /**
-     * Approach : Using Binary Search on Answers Approach
+     * Approach II : Using Hashing Approach
+     * 
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
+     */
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        // H-Index can never be more than 'n'
+        int[] bucket = new int[n + 1]; // SC: O(N)
+        for (int c : citations) { // TC: O(N)
+            if (c >= n) {
+                bucket[n]++;
+            } else {
+                bucket[c]++;
+            }
+        }
+        int cumulativeSum = 0;
+        for (int i = n; i >= 0; i--) { // TC: O(N)
+            cumulativeSum += bucket[i];
+            if (cumulativeSum >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Approach I : Using Binary Search on Answers Approach
      * 
      * TC: O(N) + O(N x log(N)) ~ O(N x log(N))
      * SC: O(1)
      */
-    public int hIndex(int[] citations) {
-        int n = citations.length;
+    public int hIndexBinarySearch(int[] citations) {
         int low = 0;
         int high = Integer.MIN_VALUE;
         for (int x : citations) { // TC: O(N)
