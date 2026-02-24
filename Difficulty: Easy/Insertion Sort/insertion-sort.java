db5e423b489c5d891fecb7b363f5af527ebdac66
Surajit Rana <surajit1602@gmail.com>
2026-02-25 00:54:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.java
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -1,20 +1,20 @@
 class Solution {
     // Please change the array in-place
     /**
-     * Approach : Using Optimal (Insertion Sort) Approach
+     * Approach : Using Insertion Sort Approach
      * 
-     * TC: O(N²), Best Case is O(N)
+     * TC: O(N²), Best Case: O(N)
      * SC: O(1)
      */
     public void insertionSort(int arr[]) {
         int n = arr.length;
-        for (int i = 1; i < n; i++) { // TC: O(N)
+        for (int i = 1; i < n; i++) {     // TC: O(N)
             int j = i;
             while (j > 0 && arr[j] < arr[j - 1]) { // TC: O(N)
-                // swap arr[j] with arr[j - 1] to place arr[j] at its sorted position
-                int temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
+                // swap arr[j] with arr[j - 1]
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
                 j--;
             }
         }
