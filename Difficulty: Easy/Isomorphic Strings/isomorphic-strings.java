2376ad07353941bdfce8ac502141c75cccf635d9
Surajit Rana <surajit1602@gmail.com>
2026-02-26 00:31:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * Approach : Using Hashing Approach
+     * 
+     * TC: O(N)
+     * SC: O(26) + O(26) ~ O(1)
+     */
+    public boolean areIsomorphic(String s1, String s2) {
+        int n = s1.length();
+        // Map size is 26 as s1 and s2 consists of only lowercase English letters
+        Map<Integer, Integer> map = new HashMap<>();    // SC: O(26)
+        Map<Integer, Integer> revMap = new HashMap<>(); // SC: O(26)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int s1key = s1.charAt(i) - 'a';
+            int s2key = s2.charAt(i) - 'a';
+            if (!map.containsKey(s1key)) {
+                map.put(s1key, s2key);
+            } else {
+                if (s2key != map.get(s1key)) {
+                    return false;
+                }
+            }
+            if (!revMap.containsKey(s2key)) {
+                revMap.put(s2key, s1key);
+            } else {
+                if (s1key != revMap.get(s2key)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
