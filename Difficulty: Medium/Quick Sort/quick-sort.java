cbe5e9c1bf495b7df09f5b5b4ba6713afd919241
Surajit Rana <surajit1602@gmail.com>
2026-02-26 02:11:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -1,23 +1,20 @@
 class Solution {
     /**
-     * Approach : Using Quick Sorting (Divide and Conquer) Approach
+     * Approach : Using Quick Sort Approach
      * 
      * TC: O(N x log(N))
      * SC: O(log(N))
-     * 
      * - O(log(N)) - recursion stack
      */
     public void quickSort(int[] arr, int low, int high) {
         if (low >= high) {
             /**
-			 * invalid pointers or if low equals high, pointers 
-			 * pointing to same element which is sorted by itself
-			 */
-			return;
+             * out of bounds or single element is 
+             * left which is by default sorted
+             */
+            return;
         }
-        // this method will return the pivot index after placing it in correct position
-        // Complexity -> T(n) = O(N) + 2 x T(n / 2)
-        int pivotIdx = partition(arr, low, high); // TC: O(N)
+        int pivotIdx = partition(arr, low, high); // TC: O(N), SC: O(1)
         quickSort(arr, low, pivotIdx - 1);
         quickSort(arr, pivotIdx + 1, high);
     }
@@ -36,21 +33,20 @@ class Solution {
             while (i <= j && arr[i] <= arr[pivotIdx]) {
                 i++;
             }
-            // here the arr[i] > arr[pivotIdx]
             while (i <= j && arr[j] >= arr[pivotIdx]) {
                 j--;
             }
-            // here the arr[j] < arr[pivotIdx]
-            if (i < j) {
+            if (i <= j) {
                 // swap arr[i] with arr[j]
-                swap(arr, i, j); // TC: O(1), SC: O(1)
+                swap(arr, i, j); // TC: O(1)
             }
         }
-        // here i > j, so we need to place arr[pivotIdx] to its actual position j
-        swap(arr, pivotIdx, j); // TC: O(1), SC: O(1)
+        // at this place i > j, so swap arr[pivotIdx] with arr[j]
+        swap(arr, pivotIdx, j);  // TC: O(1)
+        // return position of j which is the correct position of arr[pivotIdx]
         return j;
     }
-    
+
     /**
      * Using Enumeration Approach
      * 
@@ -58,8 +54,8 @@ class Solution {
      * SC: O(1)
      */
     private void swap(int[] arr, int p, int q) {
-        int temp = arr[q];
-        arr[q] = arr[p];
-        arr[p] = temp;
-    } 
+        int temp = arr[p];
+        arr[p] = arr[q];
+        arr[q] = temp;
+    }
 }
