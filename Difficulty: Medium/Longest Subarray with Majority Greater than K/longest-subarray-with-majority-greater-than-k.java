127fb1f7cd81497ebd51bd55a2ac0098cfd1e6d4
Surajit Rana <surajit1602@gmail.com>
2026-02-25 11:31:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -1,43 +1,35 @@
 class Solution {
     /**
-     * Approach : Using Monotonic Stack Approach
+     * Approach : Using Hashing Approach
      * 
-     * TC: O(N) + O(N) + O(N) + O(N) ~ O(N)
-     * SC: O(N) + O(N) ~ O(N)
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
      */
     public int longestSubarray(int[] arr, int k) {
         int n = arr.length;
         /**
-         * Intuition: we can replace elements > k with +1 and 
-         * elements <= k with -1
+         * we will modify the input array 'arr' and
+         * replace if arr[i] > k with +1 and if arr[i]
+         * <= k with -1
          */
-        for (int i = 0; i < n; i++) {      // TC: O(N)
-            arr[i] = arr[i] > k ? 1 : -1;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            arr[i] = arr[i] > k ? 1 : -1; 
         }
-        /**
-         * building prefix array
-         */
-        int[] prefix = new int[n];         // SC: O(N)
-        prefix[0] = arr[0];
-        for (int i = 1; i < n; i++) {      // TC: O(N)
-            prefix[i] = prefix[i - 1] + arr[i];
-        }
-        // we will store indices in Stack to form Monotonic Decreasing Stack
-        Stack<Integer> st = new Stack<>(); // SC: O(N)
-        for (int i = 0; i < n; i++) {      // TC: O(N)
-            if (st.isEmpty() || prefix[i] < prefix[st.peek()]) {
-                st.push(i);
-            }
-        }
-        // now traverse from right to left to find the prefix[r] > prefix[l - 1]
+        int prefixSum = 0;
         int maxLength = 0;
-        for (int r = n - 1; r >= 0; r--) {  // TC: O(N)
-            if (prefix[r] > 0) {
-                maxLength = Math.max(maxLength, r + 1);
-            }
-            while (!st.isEmpty() && prefix[r] > prefix[st.peek()]) {
-                maxLength = Math.max(maxLength, r - st.peek());
-                st.pop();
+        Map<Integer, Integer> prefixMap = new HashMap<Integer, Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            prefixSum += arr[i];
+            if (prefixSum > 0) {
+                maxLength = i + 1;
+            } else {
+                if (!prefixMap.containsKey(prefixSum)) {
+                    prefixMap.put(prefixSum, i);
+                }
+                int diff = prefixSum - 1;
+                if (prefixMap.containsKey(diff)) {
+                    maxLength = Math.max(maxLength, i - prefixMap.get(diff));
+                }
             }
         }
         return maxLength;
