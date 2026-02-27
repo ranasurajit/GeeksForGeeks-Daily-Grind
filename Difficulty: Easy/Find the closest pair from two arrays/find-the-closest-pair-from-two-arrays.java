0a1dafa15b70f48dad9d2c140e2a3dd0d08e3e57
Surajit Rana <surajit1602@gmail.com>
2026-02-28 00:33:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the closest pair from two arrays/find-the-closest-pair-from-two-arrays.java
+++ Difficulty: Easy/Find the closest pair from two arrays/find-the-closest-pair-from-two-arrays.java
@@ -1,43 +1,45 @@
 class Solution {
     /**
-     * Approach : Using Simulation Approach
+     * Approach : Using Two Pointers Approach
      * 
-     * TC: O(Min(N, M))
-     * SC: O(Min(N, M))
+     * TC: O(N + M)
+     * SC: O(1)
      */
     public static ArrayList<Integer> findClosestPair(int arr1[], int arr2[], int x) {
         int n = arr1.length;
         int m = arr2.length;
         int p = 0;     // pointer at the start of the arr1
         int q = m - 1; // pointer at the end of the arr2
+        int pair1 = -1;
+        int pair2 = -1;
+        int minDiff = Integer.MAX_VALUE;
         ArrayList<Integer> result = new ArrayList<>();
-        ArrayList<int[]> pairs = new ArrayList<>(); // SC: O(Min(N, M))
-        while (p < n && q >= 0) { // TC: O(Min(N, M))
-            if (arr1[p] + arr2[q] == x) {
-                result.add(arr1[p]);
-                result.add(arr2[q]);
-                return result;
-            } else if (arr1[p] + arr2[q] < x) {
+        while (p < n && q >= 0) { // TC: O(N + M)
+            int sum = arr1[p] + arr2[q];
+            if (sum == x) {
+                pair1 = arr1[p];
+                pair2 = arr2[q];
+                break;
+            } else if (sum < x) {
+                if (minDiff > Math.abs(sum - x)) {
+                    minDiff = Math.abs(sum - x);
+                    pair1 = arr1[p];
+                    pair2 = arr2[q];
+                }
                 // increment p
-                pairs.add(new int[] { arr1[p], arr2[q] });
                 p++;
             } else {
                 // decrement p
-                pairs.add(new int[] { arr1[p], arr2[q] });
+                if (minDiff > Math.abs(sum - x)) {
+                    minDiff = Math.abs(sum - x);
+                    pair1 = arr1[p];
+                    pair2 = arr2[q];
+                }
                 q--;
             }
         }
-        int minDiff = Integer.MAX_VALUE;
-        int[] minPair = new int[2]; // SC: O(2) ~ O(1)
-        for (int[] pair : pairs) {
-            int sum = pair[0] + pair[1];
-            if (minDiff > Math.abs(x - sum)) {
-                minDiff = Math.abs(x - sum);
-                minPair = new int[] { pair[0], pair[1] };
-            }
-        }
-        result.add(minPair[0]);
-        result.add(minPair[1]);
+        result.add(pair1);
+        result.add(pair2);
         return result;
     }
 }
