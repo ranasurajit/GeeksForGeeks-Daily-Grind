741fd72106ed271ccda055576fc6cf27b77141ec
Surajit Rana <surajit1602@gmail.com>
2026-03-02 16:14:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -1,16 +1,50 @@
 class Solution {
     /**
-     * Approach : Using Array Pre-Processing Approach
+     * Approach II : Using Two Pointers Approach
      * 
-     * TC: O(N) + O(N) + O(N) ~ O(N)
-     * SC: O(N) + O(N) ~ O(N)
+     * TC: O(N)
+     * SC: O(1)
      */
     public int maxWater(int arr[]) {
         int n = arr.length;
         /**
          * Intuition : At any index, the amount of water 
          * that can get trapped is contribution of leftMax
-         * + contribution of rightMax - height og block at
+         * + contribution of rightMax - height of block at
+         * an index
+         */
+        int trapped = 0;
+        int i = 0;      // start pointer
+        int j = n - 1;  // end pointer
+        int leftMax = arr[i];
+        int rightMax = arr[j];
+        while (i < j) { // TC: O(N)
+            leftMax = Math.max(leftMax, arr[i]);
+            rightMax = Math.max(rightMax, arr[j]);
+            if (leftMax < rightMax) {
+                // water can be contrbuted from left index
+                trapped += leftMax - arr[i];
+                i++;
+            } else {
+                trapped += rightMax - arr[j];
+                j--;
+            }
+        }
+        return trapped;
+    }
+
+    /**
+     * Approach I : Using Array Pre-Processing Approach
+     * 
+     * TC: O(N) + O(N) + O(N) ~ O(N)
+     * SC: O(N) + O(N) ~ O(N)
+     */
+    public int maxWaterUsingPrefixSuffix(int arr[]) {
+        int n = arr.length;
+        /**
+         * Intuition : At any index, the amount of water 
+         * that can get trapped is contribution of leftMax
+         * + contribution of rightMax - height of block at
          * an index
          */
         int[] leftMax = new int[n];        // SC: O(N)
