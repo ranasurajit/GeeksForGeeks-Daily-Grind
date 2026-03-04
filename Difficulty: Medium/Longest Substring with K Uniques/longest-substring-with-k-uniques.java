ebdb3893eaadea8761391e2c5495db9c60af9625
Surajit Rana <surajit1602@gmail.com>
2026-03-05 00:29:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * Approach : Using Sliding Window (Variable Size) Approach
+     * 
+     * TC: O(N)
+     * SC: O(K)
+     */
+    public int longestKSubstr(String s, int k) {
+        int n = s.length();
+        int i = 0;      // start pointer of sliding window
+        int j = 0;      // end pointer of sliding window
+        Map<Integer, Integer> freqMap = new HashMap<>(); // SC: O(K)
+        int maxLength = -1;
+        while (j < n) { // TC: O(N)
+            int chIdx = s.charAt(j) - '0';
+            freqMap.put(chIdx, freqMap.getOrDefault(chIdx, 0) + 1);
+            while (freqMap.size() > k) {
+                int removeIdx = s.charAt(i) - '0';
+                int freq = freqMap.get(removeIdx);
+                if (freq == 1) {
+                    freqMap.remove(removeIdx);
+                } else {
+                    freqMap.put(removeIdx, freq - 1);
+                }
+                i++;
+            }
+            // here the distinct characters <= k
+            if (freqMap.size() == k) {
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+            j++;
+        }
+        return maxLength;
+    }
+}
