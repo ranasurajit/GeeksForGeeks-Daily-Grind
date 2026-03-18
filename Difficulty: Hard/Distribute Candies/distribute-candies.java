0b02a44961554a9ae812dcb166fc36beecb3a7e3
Surajit Rana <surajit1602@gmail.com>
2026-03-18 12:13:25 +0530

    Update distribute-candies.java

--- Difficulty: Hard/Distribute Candies/distribute-candies.java
+++ Difficulty: Hard/Distribute Candies/distribute-candies.java
@@ -12,36 +12,94 @@ class Node {
 */
 
 class Solution {
+    private int moves = 0;
+
     /**
-     * Approach : Using DFS (Post Order Traversal) Approach
+     * Approach II : Using Optimal (DFS Post Order Traversal) Approach
      * 
      * TC: O(N)
-     * SC: O(log(N))
+     * SC: O(H) ~ O(N) in worst case
      */
     public int distCandy(Node root) {
-        if (root == null) {
-            return 0;
-        }
-        int[] moves = { 0 };
-        dfsTreeBalance(root, moves);
-        return moves[0];
+        /**
+         * each child should return how many excess/deficit
+         * candies it has/needs from its parent
+         * 
+         * so total moves = 
+         * |left excess/deficit| + |right excess/deficit|
+         */
+        dfsTreeOptimal(root);
+        return moves;
     }
     
     /**
-     * Using DFS (Post Order Traversal) Approach
+     * Using DFS Post Order Traversal Approach
      * 
      * TC: O(N)
-     * SC: O(log(N))
+     * SC: O(H)
      */
-    private int dfsTreeBalance(Node root, int[] moves) {
+    private int dfsTreeOptimal(Node root) {
+        /**
+         * This method will just return the 
+         * excess/deficit candies from children
+         * node to parent
+         */
         // Base Case
         if (root == null) {
             return 0;
         }
         // Recursion Calls
-        int leftCandiesBalance = dfsTreeBalance(root.left, moves);
-        int rightCandiesBalance = dfsTreeBalance(root.right, moves);
-        moves[0] += Math.abs(leftCandiesBalance) + Math.abs(rightCandiesBalance);
-        return root.data + leftCandiesBalance + rightCandiesBalance - 1;
+        int leftExcess = dfsTreeOptimal(root.left);
+        int rightExcess = dfsTreeOptimal(root.right);
+        moves += Math.abs(leftExcess) + Math.abs(rightExcess);
+        // this is the excess or deficit candies returned to parent node
+        return root.data - 1 + leftExcess + rightExcess;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (DFS Post Order Traversal) Approach
+     * 
+     * TC: O(N)
+     * SC: O(H) ~ O(N) in worst case
+     */
+    public int distCandyBruteForce(Node root) {
+        /**
+         * each child should return how many excess/deficit
+         * candies it has/needs from its parent
+         * 
+         * so total moves = 
+         * |left excess/deficit| + |right excess/deficit|
+         */
+        return dfsTree(root).moves;
+    }
+    
+    /**
+     * Using DFS Post Order Traversal Approach
+     * 
+     * TC: O(N)
+     * SC: O(H)
+     */
+    private Pair dfsTree(Node root) {
+        // Base Case
+        if (root == null) {
+            return new Pair(0, 0);
+        }
+        // Recursion Calls
+        Pair left = dfsTree(root.left);
+        Pair right = dfsTree(root.right);
+        int excess = root.data - 1 + left.excess + right.excess;
+        int moves = left.moves + right.moves +
+            Math.abs(left.excess) + Math.abs(right.excess);
+        return new Pair(excess, moves);
+    }
+}
+
+class Pair {
+    int excess;
+    int moves;
+    
+    public Pair(int excess, int moves) {
+        this.excess = excess;
+        this.moves = moves;
     }
 }
