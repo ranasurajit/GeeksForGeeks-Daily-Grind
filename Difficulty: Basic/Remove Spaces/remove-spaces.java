9c0371c21d83fe91834f22bb330aba9e594da0b3
Surajit Rana <surajit1602@gmail.com>
2026-04-14 13:27:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.java
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -1,11 +1,29 @@
 class Solution {
     /**
-     * Approach : Using String Simulation Approach
+     * Approach II : Using Two Pointers Approach
      * 
      * TC: O(n)
      * SC: O(n)
      */
     String removeSpaces(String s) {
+        char[] chars = s.toCharArray(); // SC: O(n)
+        int n = chars.length;
+        int write = 0;
+        for (int read = 0; read < n; read++) {
+            if (chars[read] != ' ') {
+                chars[write++] = chars[read];
+            }
+        }
+        return new String(chars, 0, write);
+    }
+
+    /**
+     * Approach I : Using String Simulation Approach
+     * 
+     * TC: O(n)
+     * SC: O(n)
+     */
+    String removeSpacesStringSimulation(String s) {
         int n = s.length();
         StringBuilder sb = new StringBuilder(); // SC: O(n)
         for (int i = 0; i < n; i++) { // TC: O(n)
