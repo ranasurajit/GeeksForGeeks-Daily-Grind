6211fd5a804089a564256f60d1e7fd038afd52d9
Surajit Rana <surajit1602@gmail.com>
2026-04-26 01:39:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -9,12 +9,13 @@ class Solution {
         int p = a.length;
         int q = b.length;
         int r = c.length;
+        int i = 0; // pointer at the start of array 'a'
+        int j = 0; // pointer at the start of array 'b'
+        int k = 0; // pointer at the start of array 'c'
         ArrayList<Integer> result = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        int k = 0;
         while (i < p && j < q && k < r) { // TC : O(p + q + r)
             if (a[i] == b[j] && b[j] == c[k]) {
+                // don't add duplicates to result
                 if (result.isEmpty() || result.get(result.size() - 1) != a[i]) {
                     result.add(a[i]);
                 }
