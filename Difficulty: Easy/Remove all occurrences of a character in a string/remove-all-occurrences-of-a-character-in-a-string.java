216a4beba2f89ef6b5bef0552243e3bd0673f3db
Surajit Rana <surajit1602@gmail.com>
2026-05-02 00:06:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove all occurrences of a character in a string/remove-all-occurrences-of-a-character-in-a-string.java
+++ Difficulty: Easy/Remove all occurrences of a character in a string/remove-all-occurrences-of-a-character-in-a-string.java
@@ -2,19 +2,57 @@
 class Solution {
     // Function to remove all occurrences of the character from the string
     /**
-     * Approach : Using Recursion Approach
+     * Approach II : Using Optimal Recursion Approach
+     * 
+     * TC : O(n)
+     * SC : O(n) + O(n)
+     * 
+     * - O(n) - forming the String result
+     * - O(n) - recursion stack
+     */
+    public void removeCharacter(StringBuilder s, char c) {
+        StringBuilder sb = new StringBuilder(); // SC : O(n)
+        solveRecursion(0, s, c, sb);
+        s.setLength(0);
+        s.append(sb);
+    }
+
+    /**
+     * Using Recursion Approach
      * 
      * TC : O(n)
      * SC : O(n)
      */
-    public void removeCharacter(StringBuilder s, char c) {
+    private void solveRecursion(int idx, StringBuilder s, char c, StringBuilder sb) {
+        // Base Case
+        if (idx >= s.length()) {
+            // nothing to process more
+            return;
+        }
+        // Recursion Calls
+        char ch = s.charAt(idx);
+        if (ch != c) {
+            sb.append(ch);
+        }
+        solveRecursion(idx + 1, s, c, sb);
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     * 
+     * TC : O(n²)
+     * SC : O(n)
+     * 
+     * - O(n) - recursion stack
+     */
+    public void removeCharacterRecursion(StringBuilder s, char c) {
         solve(0, s, c);
     }
     
     /**
      * Using Recursion Approach
      * 
-     * TC : O(n)
+     * TC : O(n²)
      * SC : O(n)
      */
     private void solve(int idx, StringBuilder s, char c) {
@@ -27,7 +65,11 @@ class Solution {
         char ch = s.charAt(idx);
         if (ch == c) {
             // removing character from s
-            s.deleteCharAt(idx);
+            s.deleteCharAt(idx); // TC : O(n)
+            /**
+             * as we deleted character at index 'idx
+             * so, next character takes its position
+             */
             solve(idx, s, c);
         } else {
             solve(idx + 1, s, c);
