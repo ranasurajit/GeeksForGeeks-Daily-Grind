902ba87dd916875da98980e2d4d7fc0c546b9c5b
Surajit Rana <surajit1602@gmail.com>
2026-05-03 20:32:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
+++ Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -2,12 +2,38 @@
 
 class Solution {
     /**
+     * Approach III : Using Math Approach
+     * 
+     * TC : O(1)
+     * SC : O(1)
+     */
+    static int sumOfNaturals(int n) {
+        return (n * (n + 1)) / 2;
+    }
+
+    /**
+     * Approach II : Using Iterative Approach
+     * 
+     * TC : O(n)
+     * SC : O(1)
+     */
+    static int sumOfNaturalsIteration(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) { // TC : O(n)
+            sum += i;
+        }
+        return sum;
+    }
+
+    /**
      * Approach I : Using Recursion Approach
      * 
      * TC : O(n)
      * SC : O(n)
+     * 
+     * - O(n) - recursion stack
      */
-    static int sumOfNaturals(int n) {
+    static int sumOfNaturalsRecursion(int n) {
         return solve(n);
     }
     
