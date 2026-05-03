fe150384ffe7015c1e3a4ff4378e2af6b04d2bf3
Surajit Rana <surajit1602@gmail.com>
2026-05-04 00:35:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.java
+++ Difficulty: Medium/Sort a stack/sort-a-stack.java
@@ -1,11 +1,66 @@
 class Solution {
     /**
-     * Approach I : Using Recursion + Iteration Approach
+     * Approach II : Using Double Recursion Approach
      * 
      * TC : O(n²)
      * SC : O(n) 
      */
     public void sortStack(Stack<Integer> st) {
+        solve(st);
+    }
+    
+    /**
+     * Using Recursion Approach
+     * 
+     * TC : O(n²)
+     * SC : O(n) 
+     */
+    private void solve(Stack<Integer> st) {
+        // Base Case
+        if (st.size() == 1) {
+            return;
+        }
+        // Recursion Calls
+        int currentItem = st.pop();
+        /**
+         * we will believe that recursion will give me
+         * the remaining stack in ascending sort
+         */
+        solve(st);
+        /**
+         * now we need to place currentItem in its 
+         * correct position in the smaller stack
+         */
+        placeInStackRecursion(st, currentItem); // TC : O(n), SC : O(n)
+    }
+    
+    /**
+     * Using Recursion Approach
+     * 
+     * TC : O(n)
+     * SC : O(n) 
+     */
+    private void placeInStackRecursion(Stack<Integer> st, int num) {
+        // Base Case
+        if (st.isEmpty() || num >= st.peek()) {
+            // no more insertion needed, add it here
+            st.push(num);
+            return;
+        }
+        // Recursion Calls
+        int current = st.pop();
+        // we believe that Stack is 
+        placeInStackRecursion(st, num);
+        st.push(current);
+    }
+
+    /**
+     * Approach I : Using Recursion + Iteration Approach
+     * 
+     * TC : O(n²)
+     * SC : O(n) 
+     */
+    public void sortStackRecursionIterativeApproach(Stack<Integer> st) {
         solveRecursion(st);
     }
     
