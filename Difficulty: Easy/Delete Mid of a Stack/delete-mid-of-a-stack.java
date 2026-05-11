e80fb54e92dacb957d12af140d3fd2649a9640f3
Surajit Rana <surajit1602@gmail.com>
2026-05-11 21:46:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.java
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.java
@@ -1,12 +1,42 @@
 class Solution {
     // Function to delete middle element of a stack.
     /**
-     * Approach : Using Recursion Approach
+     * Approach II : Using Optimal Recursion Approach
      * 
      * TC : O(n)
      * SC : O(n)
      */
     public void deleteMid(Stack<Integer> s) {
+        int n = s.size();
+        int mid = (n + 1) / 2; // 1-based
+        deleteStackMid(n, s, mid);
+    }
+    
+    /**
+     * Using Recursion Approach
+     * 
+     * TC : O(n)
+     * SC : O(n)
+     */
+    private void deleteStackMid(int n, Stack<Integer> s, int mid) {
+        // Base Case
+        if (s.size() == mid) {
+            s.pop();
+            return;
+        }
+        // Recursion Calls
+        int current = s.pop();
+        deleteStackMid(n - 1, s, mid);
+        s.push(current);
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Recursion) Approach
+     * 
+     * TC : O(n)
+     * SC : O(n)
+     */
+    public void deleteMidBruteForce(Stack<Integer> s) {
         int n = s.size();
         solve(0, n, s);
     }
