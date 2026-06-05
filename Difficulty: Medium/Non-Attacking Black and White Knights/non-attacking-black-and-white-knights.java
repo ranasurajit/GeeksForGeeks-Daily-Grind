3913a545d827e7414bd49bd9de1a07e32bf22aed
Surajit Rana <surajit1602@gmail.com>
2026-06-06 02:15:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Non-Attacking Black and White Knights/non-attacking-black-and-white-knights.java
+++ Difficulty: Medium/Non-Attacking Black and White Knights/non-attacking-black-and-white-knights.java
@@ -1,8 +1,13 @@
 class Solution {
+    private static final int[][] directions = {
+        { 1, 2 }, { -1, 2 }, { 1, -2 }, { -1, -2 },
+        { 2, 1 }, { -2, 1 }, { 2, -1 }, { -2, -1 }
+    };
+
     /**
-     * Approach : Using Math Approach
+     * Approach I : Using Backtracking Approach
      * 
-     * TC : O(1)
+     * TC : O(n x m)
      * SC : O(1)
      */
     public int numOfWays(int n, int m) {
@@ -15,17 +20,29 @@ class Solution {
          * and (n x m) - 1 ways to place the other knight
          */
         long totalWays = (n * m) * (n * m - 1);
-        long attacks = 0L;
-        /**
-         * consider n as rows so it decides vertical movement
-         * and m as columns and it decides horizontal movement
-         */
-        if (n > 2 && m > 1) {
-            attacks += 4L * (n - 2) * (m - 1);
-        }
-        if (n > 1 && m > 2) {
-            attacks += 4L * (n - 1) * (m - 2);
-        }
+        long attacks = solve(n, m); // TC : O(n x m), SC : O(1)
         return (int) (totalWays - attacks);
+    }
+    
+    /**
+     * Using Backtracking Approach
+     * 
+     * TC : O(n x m)
+     * SC : O(1)
+     */
+    private long solve(int n, int m) {
+        long attacks = 0;
+        for (int i1 = 0; i1 < n; i1++) { // TC : O(n)
+            for (int j1 = 0; j1 < m; j1++) { // TC : O(m)
+                for (int[] dir : directions) { // TC : O(8)
+                    int i2 = i1 + dir[0];
+                    int j2 = j1 + dir[1];
+                    if (i2 >= 0 && i2 < n && j2 >= 0 && j2 < m) {
+                        attacks++;
+                    }
+                }
+            }
+        }
+        return attacks;
     }
 };
