60a52078981e509e91ad7e88d446fde6ba883195
Surajit Rana <surajit1602@gmail.com>
2026-06-10 16:39:26 +0530

    Update binary-searchable-count.java

--- Difficulty: Medium/Binary Searchable Count/binary-searchable-count.java
+++ Difficulty: Medium/Binary Searchable Count/binary-searchable-count.java
@@ -1,11 +1,47 @@
 class Solution {
     /**
-     * Approach : Using Binary Search Approach
+     * Approach II : Using Optimal (Recursion) Approach
+     * 
+     * TC : O(n)
+     * SC : O(log(n))
+     */
+    public int binarySearchable(int[] arr) {
+        int n = arr.length;
+        return solve(0, n - 1, arr, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    /**
+     * Using Recursion Approach
+     * 
+     * TC : O(n)
+     * SC : O(log(n))
+     */
+    private int solve(int left, int right, int[] arr,
+        int minAllowed, int maxAllowed) {
+        // Base Case
+        if (left > right) {
+            return 0;
+        }
+        // Recursion
+        int count = 0;
+        int mid = left + (right - left) / 2;
+        if (arr[mid] > minAllowed && arr[mid] < maxAllowed) {
+            count = 1;
+        }
+        count += solve(left, mid - 1, arr,
+            minAllowed, Math.min(maxAllowed, arr[mid]));
+        count += solve(mid + 1, right, arr,
+            Math.max(minAllowed, arr[mid]), maxAllowed);
+        return count;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Binary Search) Approach
      * 
      * TC : O(n x log(n))
      * SC : O(1)
      */
-    public int binarySearchable(int[] arr) {
+    public int binarySearchableBruteForce(int[] arr) {
         int n = arr.length;
         int count = 0;
         for (int i = 0; i < n; i++) { // TC : O(n)
