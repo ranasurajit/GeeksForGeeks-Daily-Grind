476fac9ce3a38b29cd663b6189454857678d1e63
Surajit Rana <surajit1602@gmail.com>
2026-06-23 12:03:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.java
+++ Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.java
@@ -1,22 +1,47 @@
 class Solution {
     /**
-     * Approach : Using Simulation Approach
+     * Approach II : Using Binary Search Approach
+     * 
+     * TC : O(log(n))
+     * SC : O(1)
+     */
+    int maxPeopleDefeated(int p) {
+        /**
+         * Σ(n²) = (n * (n + 1) * (2 * n + 1)) / 6
+         * so, n³ ~ p so, p ranges [1 ≤ p ≤ 3 x 10⁸]
+         * so, maximum value of n ~ 1000
+         */
+        int low = 1;
+        int high = 1000;
+        while (low <= high) { // TC : O(log(n))
+            int mid = low + (high - low) / 2;
+            int sum = ((mid) * (mid + 1) * (2 * mid + 1)) / 6;
+            if (sum <= p) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+
+    /**
+     * Approach I : Using Linear (Math + Simulation) Approach
      * 
      * TC : O(p^(1/3))
      * SC : O(1)
      */
-    int maxPeopleDefeated(int p) {
-        int count = 0;
+    int maxPeopleDefeatedLinear(int p) {
         int i = 1;
-        while (p > 0) { // TC : O(p^(1/3))
-            if (i * i <= p) {
-                p -= (i * i);
+        while (true) { // TC : O(p^(1/3))
+            long square = (long) i * i;
+            if (square <= p) {
+                p -= square;
             } else {
                 break;
             }
-            count++;
             i++;
         }
-        return count;
+        return i - 1;
     }
 };
