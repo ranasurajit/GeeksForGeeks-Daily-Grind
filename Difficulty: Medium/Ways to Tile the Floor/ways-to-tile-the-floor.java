94fc802dc82eca7d58832e3998771e11d4d90ddc
Surajit Rana <surajit1602@gmail.com>
2026-06-27 01:10:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Tile the Floor/ways-to-tile-the-floor.java
+++ Difficulty: Medium/Ways to Tile the Floor/ways-to-tile-the-floor.java
@@ -2,16 +2,74 @@ class Solution {
     private static final int MOD = (int) 1e9 + 7;
     
     /**
-     * Approach II : Using Memoization Approach
+     * Approach III : Using Tabulation (Bottom-Up) Approach
+     * 
+     * TC : O(n)
+     * SC : O(n)
+     *      - O(n) - memoization memory
+     * 
+     * Accepted (1111 /1111 testcases passed)
+     */
+    public int countWays(int n, int m) {
+        if (n < m) {
+            /**
+             * tiles would not fit the height (so only 
+             * horizontal place is possible) so return 1
+             */
+            return 1;
+        }
+        if (n == m) {
+            // place all either vertically or horizontally
+            return 2;
+        }
+        long[] dp = new long[n + 1]; // SC : O(n)
+        /**
+         * Base Case 1 : n < m
+         */
+        for (int i = 0; i < m; i++) {
+            dp[i] = 1L;
+        }
+        /**
+         * Base Case 2 : n == m
+         */
+        dp[m] = 2L;
+        for (int i = m + 1; i <= n; i++) { // TC : O(n)
+            /**
+             * Option 1: Place first tile (1 x m) horizontally.
+             * 
+             * This leaves (n - 1) rows so we can rely on previous states
+             */
+            long option1 = dp[i - 1] % MOD;
+            /**
+             * Option 2: Place the first tile vertically.
+             *
+             * This leaves the first m rows with (m - 1) columns remaining.
+             * Since a horizontal tile requires all m columns, it can no longer
+             * fit inside this partially filled block.
+             *
+             * Therefore, we are forced to place the remaining (m - 1) tiles
+             * vertically as well, completely filling the first (m x m) block.
+             *
+             * The remaining floor becomes a rectangle of size
+             * (n - m) x m, so the answer is solveRecursion(n - m, m).
+             */
+            long option2 = dp[i - m] % MOD;
+            dp[i] = (option1 + option2) % MOD;
+        }
+        return (int) (dp[n] % MOD);
+    }
+
+    /**
+     * Approach II : Using Memoization (Top-Down) Approach
      * 
      * TC : O(n)
      * SC : O(n) + O(n)
      *      - O(n) - memoization memory
      *      - O(n) - recursion stack
      * 
-     * Accepted (1110 /1111 testcases passed)
+     * Accepted (1111 /1111 testcases passed)
      */
-    public int countWays(int n, int m) {
+    public int countWaysMemoization(int n, int m) {
         long[] memo = new long[n + 1]; // SC : O(n)
         Arrays.fill(memo, -1L);
         return (int) (solveMemoization(n, m, memo) % MOD);
