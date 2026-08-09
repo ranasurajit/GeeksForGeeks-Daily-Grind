2d0e684af78b8e1e6e8f9094223696d48857ea99
Surajit Rana <surajit1602@gmail.com>
2026-08-10 00:27:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/High Effort vs Low Effort/high-effort-vs-low-effort.java
+++ Difficulty: Easy/High Effort vs Low Effort/high-effort-vs-low-effort.java
@@ -1,57 +1,39 @@
 class Solution {
     /**
-     * Approach I : Using Tabulation (Bottom-Up) Approach
+     * Approach : Using Tabulation (Bottom-Up) Approach
      * 
      * TC : O(n)
-     * SC : O(n)
+     * SC : O(3 x n) ~ O(n)
      */
     public int maxTask(int[] h, int[] l) {
         int n = h.length;
         /**
-         * we can denote the type of the tasks performed as below:
-         * 0 : no task
-         * 1 : low-effort task
-         * 2 : high effort task
-         * 
-         * dp[i][t] represents the maximum number of tasks 
-         * performed on ith day
-         * 
-         * i.e. 
-         * 
-         * dp[i][0] = maximum number of tasks performed on ith day
-         * when no task is done
-         * dp[i][1] = maximum number of tasks performed on ith day
-         * when low-effort task is done
-         * dp[i][2] = maximum number of tasks performed on ith day
-         * when high-effort task is done
+         * we can consider:
+         * 0 - no task done
+         * 1 - low-effort task done
+         * 2 - high-effort task done
          */
-        int[][] dp = new int[n][3]; // SC : O(n x 3) ~ O(n)
-        dp[0][0] = 0; // no task performed on 0th day
-        dp[0][1] = l[0]; // easy task performed on 0th day
-        dp[0][2] = h[0]; // hard task performed on 0th day
+        int[][] dp = new int[n + 1][3]; // SC : O(3 x n) ~ O(n)
         /**
-         * dp[1][0] = Max(dp[0][0], dp[0][1], dp[0][2])
-         * dp[1][1] = l[1] + Max(dp[0][0], dp[0][1], dp[0][2])
+         * dp[i][0] - maximum number of tasks completed 
+         * till ith day when no task was performed 
+         * previous day
          * 
-         * if hard task is selected on 1st day then on 0th day
-         * no task should be selected
-         * dp[1][2] = h[1] + dp[0][0]
+         * dp[i][1] - maximum number of tasks completed 
+         * till ith day when low-effort task was 
+         * performed previous day
          * 
-         * dp[2][0] = Max(dp[1][0], dp[1][1], dp[1][2])
-         * dp[2][1] = l[2] + Max(dp[1][0], dp[1][1], dp[1][2])
-         * 
-         * if hard task is selected on 2nd day then on 1st day
-         * no task should be selected
-         * dp[2][2] = h[2] + dp[1][0]
+         * dp[i][2] - maximum number of tasks completed 
+         * till ith day when high-effort task was 
+         * performed previous day
          */
-        for (int i = 1; i < n; i++) { // TC : O(n)
-            dp[i][0] = Math.max(dp[i - 1][0], 
+        for (int i = 1; i <= n; i++) { // TC : O(n)
+            int maxTasksPrev = Math.max(dp[i - 1][0], 
                 Math.max(dp[i - 1][1], dp[i - 1][2]));
-            dp[i][1] = l[i] + Math.max(dp[i - 1][0], 
-                Math.max(dp[i - 1][1], dp[i - 1][2]));
-            dp[i][2] = h[i] + dp[i - 1][0];
+            dp[i][0] = maxTasksPrev;
+            dp[i][1] = maxTasksPrev + l[i - 1];
+            dp[i][2] = dp[i - 1][0] + h[i - 1];
         }
-        return Math.max(dp[n - 1][0], 
-                Math.max(dp[n - 1][1], dp[n - 1][2]));
+        return Math.max(dp[n][0], Math.max(dp[n][1], dp[n][2]));
     }
 }
